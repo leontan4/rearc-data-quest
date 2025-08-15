5a63bf9616663f360aa7d054b9ce44082a3a64d7
Leon Tan <leon.tan004@gmail.com>
2025-08-15 00:01:35 -0500

    Delete rearc directory

--- rearc/README.md
+++
@@ -1,1 +0,0 @@
-# rearc-data-quest
--- rearc/terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  type    = string
-  default = "us-east-1"
-}
-
-variable "shared_image_tag" {
-  type    = string
-  default = "initial"
-}
